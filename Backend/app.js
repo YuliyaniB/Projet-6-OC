b2afdc273b3d0c8734fff3bf8e64389132369478
YuliyaniB <166741190+YuliyaniB@users.noreply.github.com>
2024-10-01 13:42:45 +0200

    serveur.js complet

--- Backend/app.js
+++ Backend/app.js
@@ -1,18 +1,20 @@
 const express = require("express");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
+const Book = require("./models/Book");
+
+mongoose
+  .connect(
+    "mongodb+srv://julienb:julienb59200++@cluster0.d8ksr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
 
-mongoose.connect('mongodb+srv://julienb:julienb59200++@cluster0.d8ksr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    { useNewUrlParser: true,
-      useUnifiedTopology: true })
-    .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-
-
-app.use((req,res) => {
-    res.json({ message: "Votre requete a bien été reçue."})
-})
+app.use((req, res) => {
+  res.json({ message: "Votre requete a bien été reçue." });
+});
 
 module.exports = app;
