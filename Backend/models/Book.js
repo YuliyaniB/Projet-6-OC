09313d2b850391e036f61a29ad4214f780f50d18
YuliyaniB <166741190+YuliyaniB@users.noreply.github.com>
2024-10-01 13:09:25 +0200

    premier jet Schema

---
+++ Backend/models/Book.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const bookSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  year: { type: Number, required: true },
+  genre: { type: String, required: true },
+  rating: {},
+  imageUrl: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Book", bookSchema);
