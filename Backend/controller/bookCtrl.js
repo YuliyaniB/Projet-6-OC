448ba8cd6d2ac4eef661dec31153fe7413cf53bf
YuliyaniB <166741190+YuliyaniB@users.noreply.github.com>
2024-10-02 19:13:39 +0200

    fonction modifyBook

--- Backend/controller/bookCtrl.js
+++ Backend/controller/bookCtrl.js
@@ -16,7 +16,7 @@ exports.createBook = (req, res, next) => {
     const bookObject = JSON.parse(req.body.book);
     delete bookObject._id;
     delete bookObject._userId;
-    const book = new Thing({
+    const book = new Book({
         ...bookObject,
         userId: req.auth.userId,
         imageUrl: `${req.protocol}://${req.get('host')}/image/${req.file.filename}`
@@ -28,7 +28,25 @@ exports.createBook = (req, res, next) => {
 }
 
 exports.modifyBook = (req, res, next) => {
-    
+    const bookObject = req.file ? {
+        ...JSON.parse(req.body.book),
+        imageUrl: `${req.protocol}://${req.get('host')}/image/${req.file.filename}`
+    } : { ...req.body };
+  
+    delete bookObject._userId;
+    Book.findOne({_id: req.params.id})
+        .then((book) => {
+            if (book.userId != req.auth.userId) {
+                res.status(403).json({ message : '403: unauthorized request'});
+            } else {
+                Book.updateOne({ _id: req.params.id}, { ...bookObject, _id: req.params.id})
+                .then(() => res.status(200).json({message : 'Objet modifié!'}))
+                .catch(error => res.status(401).json({ error }));
+            }
+        })
+        .catch((error) => {
+            res.status(400).json({ error });
+        });
 }
 
 exports.deleteBook = (req, res, next) => {
