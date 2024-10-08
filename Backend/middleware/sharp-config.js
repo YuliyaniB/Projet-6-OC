f3ebece433b900cfe37dea18852fa0c86da42c09
YuliyaniB <166741190+YuliyaniB@users.noreply.github.com>
2024-10-08 14:32:05 +0200

    bug corrigé pour suppression image d'entrée

--- Backend/middleware/sharp-config.js
+++ Backend/middleware/sharp-config.js
@@ -4,26 +4,28 @@ const path = require("path");
 
 const optimizeImage = (req, res, next) => {
   if (!req.file) {
-    return next(); // Si aucune image n'est uploadée, on passe au middleware suivant
+    return next(); // Si aucune image n'est uploadée, passer au middleware suivant
   }
 
   const inputFilePath = req.file.path; // Chemin de l'image uploadée
   const outputFileName = req.file.filename.split('.')[0] + ".webp"; // Nom du fichier de sortie
-  const outputFilePath = path.join("image", outputFileName); // Chemin du fichier de sortie
+  const outputFilePath = path.join("image", outputFileName); // Chemin de la nouvelle image WebP
 
+  // Utiliser sharp pour redimensionner et convertir en WebP
+  sharp.cache(false)
   sharp(inputFilePath)
     .resize(400, 600) // Redimensionnement à 400x600
     .toFormat("webp") // Conversion en WebP
-    .toFile(outputFilePath) // Sauvegarde de la nouvelle image
+    .toFile(outputFilePath) // Sauvegarde de la nouvelle image WebP
     .then(() => {
-      // Suppression de l'image d'origine
-      fs.unlink(inputFilePath, (err) => {
-        if (err) {
-          console.error("Erreur lors de la suppression de l'image originale:", err);
-        }
-        // Mettre à jour les infos du fichier optimisé
-        req.file.path = outputFilePath;
-        req.file.filename = outputFileName;
+        // Supprimer l'image originale après conversion
+        fs.unlink(inputFilePath, (err) => {
+          if (err) {
+            console.error("Erreur lors de la suppression du fichier original:", err);
+          }
+          // Mettre à jour req.file avec les infos du fichier WebP
+          req.file.path = outputFilePath;
+          req.file.filename = outputFileName;
 
         next(); // Passer au middleware suivant
       });
