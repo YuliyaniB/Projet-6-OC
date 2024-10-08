428395f6cfc5c0d95792db147f517cf26bf401f9
YuliyaniB <166741190+YuliyaniB@users.noreply.github.com>
2024-10-08 16:23:19 +0200

    commentaire fonction average

--- Backend/helper/average.js
+++ Backend/helper/average.js
@@ -1,5 +1,10 @@
+// Fonction pour calculer la moyenne d'un tableau de nombres
 exports.average = (array) => {
-    if (array.length === 0) return 0;
+    // Utilise la méthode reduce pour calculer la somme des éléments du tableau.
+    // 'acc' est l'accumulateur qui commence à 0 (défini comme second argument de reduce),
+    // et 'nb' représente chaque nombre du tableau.
     const sum = array.reduce((acc, nb) => acc + nb, 0);
+    // Calcule la moyenne en divisant la somme par le nombre d'éléments,
+    // puis retourne la moyenne arrondie à une décimale (en tant que chaîne de caractères).
     return (sum / array.length).toFixed(1);
 };
